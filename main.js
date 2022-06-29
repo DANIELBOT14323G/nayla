6db9e528b04bb017e5a1d8af179aa61201c92a12
daniel o s <93184223+DANIELBOT14323G@users.noreply.github.com>
2022-06-30 05:32:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ if (!global.DATABASE.data.stats) global.DATABASE.data.stats = {}
 if (!global.DATABASE.data.msgs) global.DATABASE.data.msgs = {}
 if (!global.DATABASE.data.sticker) global.DATABASE.data.sticker = {}
 global.conn = new WAConnection()
-conn.version = [2, 2147, 16]
+conn.version = [2, 2220, 8]
 let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
