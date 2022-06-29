23c78631ff3a6daf6ebd1e66eb233465564ee13c
daniel o s <93184223+DANIELBOT14323G@users.noreply.github.com>
2022-06-30 05:50:08 +0700

    Update TQTO2.js

--- plugins/TQTO2.js
+++ plugins/TQTO2.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
 *Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
 `.trim(), m)
 }
-handler.help = ['Baka Botz','Arifi Razzaq','Xteam','RendyCraft','Ibnu NR','Caliph','Zeks']
+handler.help = ['Baka Botz','Arifi Razzaq','Xteam','RendyCraft','Ibnu NR','Caliph','Zeks,nurutomo']
 handler.tags = ['thnks']
 handler.customPrefix = /(\?$)/
 handler.command = /^rakujelek$/i
